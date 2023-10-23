66373d59ffc20871ed15a8af7f3b3788b6956f10
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-10-23 12:39:54 -0400

    Setting up file and repo

---
+++ SIM.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define NoS 1           //number of sets
+#define assoc 1         //associativity
+#define BlockSize 64    //Block Size
+
+int main()
+{
+    printf("Hello");
+}
