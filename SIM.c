c66451a036959c003ce639e0e2739aa225a01ae0
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-02 02:19:04 -0400

    updates

--- SIM.c
+++ SIM.c
@@ -1,31 +1,42 @@
+//------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 
-int NUM_SETS;
-int ASSOC;
-int CACHE_SIZE;
+//------------------------------------------------------------------
+
 #define BLOCK_SIZE 64
 
+//------------------------------------------------------------------
+
+int nos;
+int assoc;
+int CacheSize;
 long long int **tagArray;
 bool **dirty;
 
-int Hit = 0;
-int Miss = 0;
-int Write = 0;
-int Read = 0;
-int Replacement;
-int WriteBack;
+double Hits = 0;
+double Misses = 0;
 
-void UpdateLRU(long long int add);
-void UpdateFIFO(long long int add);
+int Reads = 0;
+int Writes = 0;
 
-void UpdateLRU(long long int add) {
-    int set = ((unsigned long long int)add / BLOCK_SIZE) % NUM_SETS;
-    long long int tag = add / BLOCK_SIZE;
+int rp = -1;
+int wb = -1;
+
+int set = -1;
+long long int tag = -1;
+
+//------------------------------------------------------------------
+
+void UpdateLRU( long long int add )
+{
+  int set = ((unsigned long long int)add / BLOCK_SIZE) % nos;
+  tag = add / BLOCK_SIZE;
     int i;
-    for (i = 0; i < ASSOC; i++) {
+    for (i = 0; i < assoc; i++) {
         if (tagArray[set][i] == tag) {
             break;
         }
@@ -40,11 +51,11 @@ void UpdateLRU(long long int add) {
 }
 
 void UpdateFIFO(long long int add) {
-    int set = ((unsigned long long int)add / BLOCK_SIZE) % NUM_SETS;
+    set = ((unsigned long long int)add / BLOCK_SIZE) % nos;
     long long int tag = add / BLOCK_SIZE;
-    if (dirty[set][ASSOC - 1] == true)
-        Write++;
-    for (int i = ASSOC - 1; i > 0; i--) {
+    if (dirty[set][assoc - 1] == true)
+        Writes++;
+    for (int i = assoc - 1; i > 0; i--) {
         tagArray[set][i] = tagArray[set][i - 1];
         dirty[set][i] = dirty[set][i - 1];
     }
@@ -53,52 +64,52 @@ void UpdateFIFO(long long int add) {
 }
 
 void Simulate_access(char op, long long int add) {
-    int set = ((unsigned long long int)add / BLOCK_SIZE) % NUM_SETS;
+    int set = ((unsigned long long int)add / BLOCK_SIZE) % nos;
     long long int tag = add / BLOCK_SIZE;
-    for (int i = 0; i < ASSOC; i++) {
+    for (int i = 0; i < assoc; i++) {
         if (tag == tagArray[set][i]) {
-            Hit++;
-            if (Replacement == 0) {
+            Hits++;
+            if (rp == 0) {
                 UpdateLRU(add);
-                if (op == 'W' && WriteBack == 1) {
+                if (op == 'W' && wb == 1) {
                     dirty[set][0] = true;
                 }
             } else {
-                if (op == 'W' && WriteBack == 1) {
+                if (op == 'W' && wb == 1) {
                     dirty[set][i] = true;
                 }
             }
-            if (op == 'W' && WriteBack == 0) {
-                Write++;
+            if (op == 'W' && wb == 0) {
+                Writes++;
             }
             return;
         }
     }
-    Miss++;
-    Read++;
-    if (Replacement == 0) {
-        if (dirty[set][ASSOC - 1] == true) {
-            Write++;
+    Misses++;
+    Reads++;
+    if (rp == 0) {
+        if (dirty[set][assoc - 1] == true) {
+            Writes++;
         }
-        for (int i = ASSOC - 1; i > 0; i--) {
+        for (int i = assoc - 1; i > 0; i--) {
             tagArray[set][i] = tagArray[set][i - 1];
             dirty[set][i] = dirty[set][i - 1];
         }
         tagArray[set][0] = tag;
         dirty[set][0] = false;
-        if (op == 'W' && WriteBack == 1) {
+        if (op == 'W' && wb == 1) {
             dirty[set][0] = true;
         }
-        if (op == 'W' && WriteBack == 0) {
-            Write++;
+        if (op == 'W' && wb == 0) {
+            Writes++;
         }
     } else {
         UpdateFIFO(add);
-        if (op == 'W' && WriteBack == 1) {
+        if (op == 'W' && wb == 1) {
             dirty[set][0] = true;
         }
-        if (op == 'W' && WriteBack == 0) {
-            Write++;
+        if (op == 'W' && wb == 0) {
+            Writes++;
         }
     }
 }
@@ -109,10 +120,10 @@ int main(int arg, char **args) {
         return 1;
     }
 
-    CACHE_SIZE = atoi(args[1]);
-    ASSOC = atoi(args[2]);
-    Replacement = atoi(args[3]);
-    WriteBack = atoi(args[4]);
+    CacheSize = atoi(args[1]);
+    assoc = atoi(args[2]);
+    rp = atoi(args[3]);
+    wb = atoi(args[4]);
     char *tracefile = args[5];
 
     FILE *file = fopen(tracefile, "r");
@@ -121,18 +132,18 @@ int main(int arg, char **args) {
         return 1;
     }
 
-    NUM_SETS = CACHE_SIZE / (BLOCK_SIZE * ASSOC);
+    nos = CacheSize / (BLOCK_SIZE * assoc);
 
-    tagArray = (long long int **)malloc(NUM_SETS * sizeof(long long int *));
-    dirty = (bool **)malloc(NUM_SETS * sizeof(bool *));
+    tagArray = (long long int **)malloc(nos * sizeof(long long int *));
+    dirty = (bool **)malloc(nos * sizeof(bool *));
 
-    for (int i = 0; i < NUM_SETS; i++) {
-        tagArray[i] = (long long int *)malloc(ASSOC * sizeof(long long int));
-        dirty[i] = (bool *)malloc(ASSOC * sizeof(bool));
+    for (int i = 0; i < nos; i++) {
+        tagArray[i] = (long long int *)malloc(assoc * sizeof(long long int));
+        dirty[i] = (bool *)malloc(assoc * sizeof(bool));
     }
 
-    for (int i = 0; i < NUM_SETS; i++) {
-        for (int j = 0; j < ASSOC; j++) {
+    for (int i = 0; i < nos; i++) {
+        for (int j = 0; j < assoc; j++) {
             tagArray[i][j] = -1;
             dirty[i][j] = false;
         }
@@ -146,12 +157,12 @@ int main(int arg, char **args) {
 
     fclose(file);
 
-    double Miss_ratio = (double)Miss / (Miss + Hit);
-    printf("Miss Ratio %lf\n", Miss_ratio);
-    printf("write %d\n", Write);
-    printf("read %d\n", Read);
+    double Misses_ratio = (double)Misses / (Misses + Hits);
+    printf("Misses Ratio %lf\n", Misses_ratio);
+    printf("Writes %d\n", Writes);
+    printf("Reads %d\n", Reads);
 
-    for (int i = 0; i < NUM_SETS; i++) {
+    for (int i = 0; i < nos; i++) {
         free(tagArray[i]);
         free(dirty[i]);
     }
